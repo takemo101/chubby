bd6303ac27c65f585ca574cc45851a3f151ddb86
takemo101 <takemo101@gmail.com>
2023-10-22 20:43:09 +0900

    Added tests for Http and Console's createSimple

--- tests/Application/ApplicationBuilderTest.php
+++ tests/Application/ApplicationBuilderTest.php
@@ -7,6 +7,8 @@ use Takemo101\Chubby\Bootstrap\Provider\ConsoleProvider;
 use Takemo101\Chubby\Bootstrap\Provider\DependencyProvider;
 use Takemo101\Chubby\Bootstrap\Provider\FunctionProvider;
 use Takemo101\Chubby\Bootstrap\Provider\HttpProvider;
+use Takemo101\Chubby\Console;
+use Takemo101\Chubby\Http;
 
 describe(
     'application builder',
@@ -70,6 +72,46 @@ describe(
                 ] as $excepted) {
                     expect($actualProviderClassNames)->toContain($excepted);
                 }
+            }
+        );
+
+        test(
+            "Http's createSimple method adds HttpProvider to Application via ApplicationBuilder",
+            function () {
+                $bootstrap = new Bootstrap();
+
+                Http::createSimple(
+                    ApplicationOption::from(
+                        bootstrap: $bootstrap,
+                    ),
+                );
+
+                $actualProviderClassNames = array_map(
+                    fn ($provider) => get_class($provider),
+                    $bootstrap->getProviders(),
+                );
+
+                expect($actualProviderClassNames)->toContain(HttpProvider::class);
+            }
+        );
+
+        test(
+            "Console's createSimple method adds HttpProvider to Application via ApplicationBuilder",
+            function () {
+                $bootstrap = new Bootstrap();
+
+                Console::createSimple(
+                    ApplicationOption::from(
+                        bootstrap: $bootstrap,
+                    ),
+                );
+
+                $actualProviderClassNames = array_map(
+                    fn ($provider) => get_class($provider),
+                    $bootstrap->getProviders(),
+                );
+
+                expect($actualProviderClassNames)->toContain(ConsoleProvider::class);
             }
         );
     }
