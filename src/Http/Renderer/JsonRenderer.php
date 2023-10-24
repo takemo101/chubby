6d4b651841c167534815f807fd7354856dbf399b
takemo101 <takemo101@gmail.com>
2023-10-24 10:30:51 +0900

    Refactor ResponseRenderer

--- src/Http/Renderer/JsonRenderer.php
+++ src/Http/Renderer/JsonRenderer.php
@@ -2,56 +2,26 @@
 
 namespace Takemo101\Chubby\Http\Renderer;
 
-use Fig\Http\Message\StatusCodeInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Takemo101\Chubby\Contract\Arrayable;
 
-final class JsonRenderer implements ResponseRenderer
+final class JsonRenderer extends AbstractRenderer
 {
+    /** @var string */
+    public const ContentType = 'application/json';
+
     /**
-     * constructor
+     * Get response body content to be rendered.
      *
      * @param mixed $data
-     * @param int $status
-     * @param array<string,string> $headers
+     * @return string
      */
-    public function __construct(
-        private mixed $data,
-        private int $status = StatusCodeInterface::STATUS_OK,
-        private array $headers = []
-    ) {
-        //
-    }
-
-    /**
-     * Perform response writing process.
-     *
-     * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @return ResponseInterface
-     */
-    public function render(
-        ServerRequestInterface $request,
-        ResponseInterface $response,
-    ): ResponseInterface {
-        $response = $response
-            ->withStatus($this->status)
-            ->withHeader('Content-Type', 'application/json');
-
-        foreach ($this->headers as $key => $value) {
-            $response = $response->withHeader($key, $value);
-        }
-
-        $response->getBody()->write(
-            (string)json_encode(
-                $this->data instanceof Arrayable
-                    ? $this->data->toArray()
-                    : $this->data,
-                JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR
-            )
+    protected function getContent(mixed $data): string
+    {
+        return (string)json_encode(
+            $data instanceof Arrayable
+                ? $data->toArray()
+                : $data,
+            JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR
         );
-
-        return $response;
     }
 }
--- src/Http/Renderer/StringRenderer.php
+++ src/Http/Renderer/StringRenderer.php
@@ -2,60 +2,20 @@
 
 namespace Takemo101\Chubby\Http\Renderer;
 
-use Fig\Http\Message\StatusCodeInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Takemo101\Chubby\Contract\Renderable;
 
-class StringRenderer implements ResponseRenderer
+class StringRenderer extends AbstractRenderer
 {
-    /** @var string */
-    public const ContentType = 'text/plain';
-
     /**
-     * constructor
+     * Get response body content to be rendered.
      *
      * @param mixed $data
-     * @param int $status
-     * @param array<string,string> $headers
+     * @return string
      */
-    public function __construct(
-        private mixed $data,
-        private int $status = StatusCodeInterface::STATUS_OK,
-        private array $headers = []
-    ) {
-        //
-    }
-
-    /**
-     * Perform response writing process.
-     *
-     * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @return ResponseInterface
-     */
-    public function render(
-        ServerRequestInterface $request,
-        ResponseInterface $response,
-    ): ResponseInterface {
-        $response = $response
-            ->withStatus($this->status)
-            ->withHeader('Content-Type', static::ContentType);
-
-        foreach ($this->headers as $key => $value) {
-            $response = $response->withHeader($key, $value);
-        }
-
-        $data = $this->data;
-
-        if ($data instanceof Renderable) {
-            $data = $data->render();
-        }
-
-        $response->getBody()->write(
-            (string) $data, // @phpstan-ignore-line
-        );
-
-        return $response;
+    protected function getContent(mixed $data): string
+    {
+        return $data instanceof Renderable
+            ? $data->render()
+            : (string) $data; // @phpstan-ignore-line
     }
 }
