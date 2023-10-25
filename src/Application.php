548df02728cd82c9af9223f25244eca21a8d43a4
takemo101 <takemo101@gmail.com>
2023-10-25 14:59:45 +0900

    Change application version

--- src/Application.php
+++ src/Application.php
@@ -43,7 +43,7 @@ final class Application implements ApplicationContainer
     /**
      * @var string
      */
-    public const Version = '0.1.0';
+    public const Version = '0.0.5';
 
     /**
      * @var Container|null
