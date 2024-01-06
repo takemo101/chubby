71d0d44162780e0cafbd363bff061b5e99c603e3
takemo101 <takemo101@gmail.com>
2024-01-06 09:48:37 +0900

    Refactor DefinitionHelperTest.php

--- tests/Bootstrap/DefinitionHelperTest.php
+++ tests/Bootstrap/DefinitionHelperTest.php
@@ -10,103 +10,105 @@ describe(
     'DefinitionHelper',
     function () {
 
-        beforeEach(function () {
-            $this->config = m::mock(ConfigRepository::class);
-            $this->container = m::mock(ContainerInterface::class);
-            $this->hook = m::mock(Hook::class);
-        });
+        describe(
+            'createReplaceableDefinition',
+            function () {
 
-        afterEach(function () {
-            m::close();
-        });
+                beforeEach(function () {
+                    $this->config = m::mock(ConfigRepository::class);
+                    $this->container = m::mock(ContainerInterface::class);
+                    $this->hook = m::mock(Hook::class);
+                });
 
-        it('should return a closure', function () {
-            $entry = 'MyEntry';
-            $configKey = 'my_entry_config';
-            $defaultClass = 'DefaultClass';
-            $hook = false;
+                it('should return a closure', function () {
+                    $entry = 'MyEntry';
+                    $configKey = 'my_entry_config';
+                    $defaultClass = 'DefaultClass';
+                    $hook = false;
 
-            $closure = DefinitionHelper::createReplaceableDefinition(
-                $entry,
-                $configKey,
-                $defaultClass,
-                $hook
-            );
+                    $closure = DefinitionHelper::createReplaceableDefinition(
+                        $entry,
+                        $configKey,
+                        $defaultClass,
+                        $hook
+                    );
 
-            expect($closure)->toBeInstanceOf(Closure::class);
-        });
+                    expect($closure)->toBeInstanceOf(Closure::class);
+                });
 
-        it('should return the instance from the container without hooking', function () {
-            $entry = 'MyEntry';
-            $configKey = 'my_entry_config';
-            $defaultClass = 'DefaultClass';
-            $hook = false;
+                it('should return the instance from the container without hooking', function () {
+                    $entry = 'MyEntry';
+                    $configKey = 'my_entry_config';
+                    $defaultClass = 'DefaultClass';
+                    $hook = false;
 
-            $class = 'MyClass';
-            $instance = new stdClass();
+                    $class = 'MyClass';
+                    $instance = new stdClass();
 
-            $this->config->shouldReceive('get')
-                ->once()
-                ->with($configKey, $defaultClass)
-                ->andReturn($class);
+                    $this->config->shouldReceive('get')
+                        ->once()
+                        ->with($configKey, $defaultClass)
+                        ->andReturn($class);
 
-            $this->container->shouldReceive('get')
-                ->once()
-                ->with($class)
-                ->andReturn($instance);
+                    $this->container->shouldReceive('get')
+                        ->once()
+                        ->with($class)
+                        ->andReturn($instance);
 
-            $closure = DefinitionHelper::createReplaceableDefinition(
-                $entry,
-                $configKey,
-                $defaultClass,
-                $hook
-            );
+                    $closure = DefinitionHelper::createReplaceableDefinition(
+                        $entry,
+                        $configKey,
+                        $defaultClass,
+                        $hook
+                    );
 
-            $result = $closure($this->config, $this->container);
+                    $result = $closure($this->config, $this->container);
 
-            expect($result)->toBe($instance);
-        });
+                    expect($result)->toBe($instance);
+                });
 
-        it('should return the instance from the container with hooking', function () {
-            $entry = 'MyEntry';
-            $configKey = 'my_entry_config';
-            $defaultClass = 'DefaultClass';
-            $hook = true;
+                it('should return the instance from the container with hooking', function () {
+                    $entry = 'MyEntry';
+                    $configKey = 'my_entry_config';
+                    $defaultClass = 'DefaultClass';
+                    $hook = true;
 
-            $class = 'MyClass';
-            $instance = new stdClass();
-            $hookedInstance = new stdClass();
+                    $class = 'MyClass';
+                    $instance = new stdClass();
+                    $hookedInstance = new stdClass();
 
-            $this->config->shouldReceive('get')
-                ->once()
-                ->with($configKey, $defaultClass)
-                ->andReturn($class);
+                    $this->config->shouldReceive('get')
+                        ->once()
+                        ->with($configKey, $defaultClass)
+                        ->andReturn($class);
 
-            $this->container->shouldReceive('get')
-                ->once()
-                ->with($class)
-                ->andReturn($instance);
+                    $this->container->shouldReceive('get')
+                        ->once()
+                        ->with($class)
+                        ->andReturn($instance);
 
-            $this->container->shouldReceive('get')
-                ->once()
-                ->with(Hook::class)
-                ->andReturn($this->hook);
+                    $this->container->shouldReceive('get')
+                        ->once()
+                        ->with(Hook::class)
+                        ->andReturn($this->hook);
 
-            $this->hook->shouldReceive('do')
-                ->once()
-                ->with($entry, $instance)
-                ->andReturn($hookedInstance);
+                    $this->hook->shouldReceive('do')
+                        ->once()
+                        ->with($entry, $instance)
+                        ->andReturn($hookedInstance);
 
-            $closure = DefinitionHelper::createReplaceableDefinition(
-                $entry,
-                $configKey,
-                $defaultClass,
-                $hook
-            );
+                    $closure = DefinitionHelper::createReplaceableDefinition(
+                        $entry,
+                        $configKey,
+                        $defaultClass,
+                        $hook
+                    );
 
-            $result = $closure($this->config, $this->container);
+                    $result = $closure($this->config, $this->container);
 
-            expect($result)->toBe($hookedInstance);
-        });
+                    expect($result)->toBe($hookedInstance);
+                });
+            }
+        )->group('DefinitionHelper', 'bootstrap');
     }
-)->group('DefinitionHelper', 'bootstrap');
+);
