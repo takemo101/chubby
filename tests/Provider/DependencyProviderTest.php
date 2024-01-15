64ccb46473cd41acd2e0cb666d59b881f54a67fc
takemo101 <takemo101@gmail.com>
2024-01-15 10:24:47 +0900

    Refactor dependency registration in DependencyProviderTest

--- tests/Provider/DependencyProviderTest.php
+++ tests/Provider/DependencyProviderTest.php
@@ -14,10 +14,36 @@ beforeEach(function () {
     $this->provider = new DependencyProvider($this->path, $this->filesystem);
 });
 
-it('should set the dependency path', function () {
-    $path = 'path/to/dependency.php';
+it('should register dependency definitions', function () {
+    $definitions = m::spy(Definitions::class);
 
-    $this->provider->setDependencyPath($path);
+    $dependencyPath1 = 'path/to/dependency1.php';
+    $dependencyPath2 = 'path/to/dependency2.php';
 
-    expect($this->provider->getDependencyPath())->toBe($path);
+    $this->filesystem->shouldReceive('exists')
+        ->with($dependencyPath1)
+        ->andReturn(true);
+    $this->filesystem->shouldReceive('require')
+        ->with($dependencyPath1)
+        ->andReturn(['key1' => 'value1']);
+    $this->filesystem->shouldReceive('exists')
+        ->with($dependencyPath2)
+        ->andReturn(true);
+    $this->filesystem->shouldReceive('require')
+        ->with($dependencyPath2)
+        ->andReturn(['key2' => 'value2']);
+
+    $this->provider->setDependencyPaths($dependencyPath1, $dependencyPath2);
+
+    expect($this->provider->getDependencyPaths())
+        ->toBe([$dependencyPath1, $dependencyPath2]);
+
+    $this->provider->register($definitions);
+
+    $definitions->shouldHaveReceived('add')
+        ->once()
+        ->with([
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ]);
 })->group('DependencyProvider', 'provider');
