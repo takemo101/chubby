a09bc30e6f8c6ada83b38dcd795491e6ecbacbda
takemo101 <takemo101@gmail.com>
2024-06-11 13:57:08 +0900

    feat: Remove StreamFactoryInjectable interface and related code
    This commit removes the `StreamFactoryInjectable` interface and its related code from the project. The interface was no longer being used and was deemed unnecessary. This change simplifies the codebase and removes unused dependencies.

--- tests/Http/ResponseTransformer/InjectableFilterTest.php
+++ tests/Http/ResponseTransformer/InjectableFilterTest.php
@@ -5,36 +5,17 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Takemo101\Chubby\Http\ResponseTransformer\InjectableFilter;
 use Takemo101\Chubby\Contract\ContainerInjectable;
-use Takemo101\Chubby\Contract\StreamFactoryInjectable;
 use Takemo101\Chubby\ApplicationContainer;
 use Mockery as m;
 
 beforeEach(function () {
     $this->container = m::mock(ApplicationContainer::class);
-    $this->streamFactory = m::mock(StreamFactoryInterface::class);
-    $this->filter = new InjectableFilter($this->container, $this->streamFactory);
+    $this->filter = new InjectableFilter($this->container);
 });
 
 describe(
     'InjectableFilter',
     function () {
-
-        it(
-            'should set the stream factory if the data is StreamFactoryInjectable',
-            function () {
-                $data = m::mock(StreamFactoryInjectable::class);
-                $data->shouldReceive('setStreamFactory')
-                    ->once()
-                    ->with($this->streamFactory);
-
-                $request = m::mock(ServerRequestInterface::class);
-                $response = m::mock(ResponseInterface::class);
-
-                $result = $this->filter->transform($data, $request, $response);
-
-                expect($result)->toBeNull();
-            }
-        );
 
         it(
             'should set the container if the data is ContainerInjectable',
