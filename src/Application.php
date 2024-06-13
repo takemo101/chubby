d497eecfbaa21da3cad12322f6fbc4ad0468769b
takemo101 <takemo101@gmail.com>
2024-06-13 12:20:21 +0900

    Update Application version to 0.1.1

--- src/Application.php
+++ src/Application.php
@@ -51,7 +51,7 @@ class Application implements ApplicationContainer
     /**
      * @var string
      */
-    public const Version = '0.0.27';
+    public const Version = '0.1.1';
 
     /**
      * @var Container|null
