4d62fb0da873d38aa68ad1fac89705be33790027
takemo101 <takemo101@gmail.com>
2024-06-18 11:53:28 +0900

    refactor: Remove unused DI\Factory\RequestedEntry import in ContextProvider.php

--- config/slim.php
+++ config/slim.php
@@ -2,26 +2,13 @@
 
 // Slim framework configuration
 
-use Takemo101\Chubby\Http\Configurer\DefaultSlimConfigurer;
-use Takemo101\Chubby\Http\ErrorHandler\ErrorHandler;
-use Takemo101\Chubby\Http\Factory\DefaultSlimFactory;
-
 return [
 
     // Base path
     'base_path' => env('BASE_PATH'),
 
-    // Specify a class that implements Slimfactory, a factor class that generates Slim
-    'factory' => DefaultSlimFactory::class,
-
-    // Specify a class that implements Slimconfigurer to perform setting processing before executing Slim
-    'configurer' => DefaultSlimConfigurer::class,
-
     // Error output settings
     'error' => [
-
-        // Slim error handling specified classes that implement Error HandlerInterface
-        'handler' => ErrorHandler::class,
 
         // ErrorMiddleware error display setting
         'setting' => [
