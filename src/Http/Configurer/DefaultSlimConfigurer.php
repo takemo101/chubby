9c83eaa338b06f987da99433e9e1001a08fbdcf2
takemo101 <takemo101@gmail.com>
2024-06-19 15:12:15 +0900

    refactor: Add Slim configuration hooks for before and after middleware addition

--- src/Http/Configurer/DefaultSlimConfigurer.php
+++ src/Http/Configurer/DefaultSlimConfigurer.php
@@ -6,6 +6,9 @@ use DI\Attribute\Inject;
 use Slim\App as Slim;
 use Slim\Middleware\BodyParsingMiddleware;
 use Slim\Middleware\ErrorMiddleware;
+use Takemo101\Chubby\ApplicationHookTags;
+use Takemo101\Chubby\Event\EventDispatcher;
+use Takemo101\Chubby\Hook\Hook;
 use Takemo101\Chubby\Http\Middleware\StartContext;
 
 class DefaultSlimConfigurer implements SlimConfigurer
@@ -18,9 +21,13 @@ class DefaultSlimConfigurer implements SlimConfigurer
     /**
      * constructor
      *
+     * @param EventDispatcher $dispatcher
+     * @param Hook $hook
      * @param string|null $basePath
      */
     public function __construct(
+        private readonly EventDispatcher $dispatcher,
+        private readonly Hook $hook,
         #[Inject('config.slim.base_path')]
         ?string $basePath = null,
     ) {
@@ -35,14 +42,35 @@ class DefaultSlimConfigurer implements SlimConfigurer
      */
     public function configure(Slim $slim): Slim
     {
+        $this->hook->do(
+            tag: ApplicationHookTags::Http_BeforeSlimConfiguration,
+            parameter: $slim,
+        );
+
         if ($this->basePath) {
             $slim->setBasePath($this->basePath);
         }
 
+        $this->hook->do(
+            tag: ApplicationHookTags::Http_BeforeAddRoutingMiddleware,
+            parameter: $slim,
+        );
+
         $slim->addRoutingMiddleware();
+
+        $this->hook->do(
+            tag: ApplicationHookTags::Http_AfterAddRoutingMiddleware,
+            parameter: $slim,
+        );
+
         $slim->add(BodyParsingMiddleware::class);
         $slim->add(ErrorMiddleware::class);
         $slim->add(StartContext::class);
+
+        $this->hook->do(
+            tag: ApplicationHookTags::Http_AfterSlimConfiguration,
+            parameter: $slim,
+        );
 
         return $slim;
     }
