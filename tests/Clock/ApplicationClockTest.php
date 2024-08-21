ea0bbf049dd45639bd47d045cdd3d8a68cf82155
takemo101 <takemo101@gmail.com>
2024-08-21 15:34:35 +0900

    refactor: Add timezone functionality to ApplicationClock tests

--- tests/Clock/ApplicationClockTest.php
+++ tests/Clock/ApplicationClockTest.php
@@ -31,5 +31,25 @@ describe(
 
             expect($currentTimeString)->toMatch('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/');
         });
+
+        it('returns a new instance with the specified timezone', function () {
+            $timezone = new DateTimeZone('Europe/London');
+            $clock = new ApplicationClock(new DateTimeZone('UTC'));
+
+            $newClock = $clock->withTimezone($timezone);
+
+            expect($newClock)->toBeInstanceOf(ApplicationClock::class);
+            expect($newClock->now()->getTimezone())->toEqual($timezone);
+        });
+
+        it('returns the timezone set in the clock', function () {
+            $timezone = new DateTimeZone('Asia/Tokyo');
+            $clock = new ApplicationClock($timezone);
+
+            $clockTimezone = $clock->getTimezone();
+
+            expect($clockTimezone)->toEqual($timezone);
+        });
+
     },
 )->group('ApplicationClock', 'clock');
