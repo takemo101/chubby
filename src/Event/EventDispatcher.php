1953801f4a941b454cf70150d854ace3aff42e2d
takemo101 <takemo101@gmail.com>
2024-08-22 13:30:42 +0900

    refactor: Improve error handling in EventDispatcher class

--- src/Event/EventDispatcher.php
+++ src/Event/EventDispatcher.php
@@ -4,6 +4,8 @@ namespace Takemo101\Chubby\Event;
 
 use Psr\EventDispatcher\StoppableEventInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface as SymfonyEventDispatcherInterface;
+use Takemo101\Chubby\Exception\Exceptions;
+use Throwable;
 
 class EventDispatcher implements SymfonyEventDispatcherInterface
 {
@@ -20,6 +22,8 @@ class EventDispatcher implements SymfonyEventDispatcherInterface
 
     /**
      * {@inheritDoc}
+     *
+     * @throws Exceptions
      */
     public function dispatch(object $event, ?string $eventName = null): object
     {
@@ -27,17 +31,45 @@ class EventDispatcher implements SymfonyEventDispatcherInterface
             ? $this->provider->getListeners($eventName)
             : $this->provider->getListenersForEvent($event);
 
-        foreach ($listeners as $listener) {
-            call_user_func($listener, $event);
-
-            if (
-                $event instanceof StoppableEventInterface
-                && $event->isPropagationStopped()
-            ) {
-                break;
-            }
+        // If an exception occurs during the listener call, throw the exception.
+        if ($exceptions = $this->handleListeners($listeners, $event)) {
+            throw $exceptions;
         }
 
         return $event;
+    }
+
+    /**
+     * Executes all listener functions in the listener array and returns an exception if any occur.
+     * Returns null if no exceptions occur.
+     *
+     * If an exception occurs in a specific listener, it interrupts the execution of that listener and proceeds to the next one.
+     *
+     * @param iterable<callable> $listeners
+     * @param object $event
+     * @return Exceptions|null
+     */
+    private function handleListeners(iterable $listeners, object $event): ?Exceptions
+    {
+        $throwables = [];
+
+        foreach ($listeners as $listener) {
+            try {
+                call_user_func($listener, $event);
+
+                if (
+                    $event instanceof StoppableEventInterface
+                    && $event->isPropagationStopped()
+                ) {
+                    break;
+                }
+            } catch (Throwable $e) {
+                $throwables[] = $e;
+            }
+        }
+
+        return empty($throwables) === false
+            ? new Exceptions(...$throwables)
+            : null;
     }
 }
